132d94ef67a3eeed4eb3c35801a53e5d668e1cef
bluefog <bluefoginthecup@gmail.com>
2022-09-11 00:13:45 -0700

    create Readme.md

--- prac_json.js
+++ prac_json.js
@@ -1,4 +1,5 @@
-fetch("https://github.com/bluefoginthecup/test2.git/db.json").then((response) => response.text()).then((result) => {
+fetch("https://github.com/bluefoginthecup/test2.git/db.json")
+.then((response) => response.text()).then((result) => {
     const users = JSON.parse(result);
     console.log(users);
 });
